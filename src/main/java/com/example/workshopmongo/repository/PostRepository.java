d1750a7c70a02cd2cea5b466edb4298754471a51
Mauricio Yuji <mauricio.nakandakari2@gmail.com>
2020-08-15 12:52:43 -0300

    Consulta simples com @Query

--- src/main/java/com/example/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/example/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.example.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.workshopmongo.domain.Post;
@@ -11,6 +12,10 @@ import com.example.workshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 	
 	//Query Mwthods
+	
+	@Query("{ title: { $regex: ?0, $options: 'i' } }") //?0 aproveitar o valor do parametro "text"
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 	
