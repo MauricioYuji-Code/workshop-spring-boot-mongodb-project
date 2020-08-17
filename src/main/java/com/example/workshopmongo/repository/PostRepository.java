84cd39477ae84fb36d48162b9b97838a37a6f389
Mauricio Yuji <mauricio.nakandakari2@gmail.com>
2020-08-17 11:34:24 -0300

    consulta com varios criterios

--- src/main/java/com/example/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/example/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.example.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -17,6 +18,10 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+	//gte -> Maior ou igual lt -> Menor ou igual
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);// primeiro parametro 0?, segundo 1? e terceiro 3?
 	
 	
 
